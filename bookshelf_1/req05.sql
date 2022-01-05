547e5ec655ac635b3084ff8664e7ba80a344194a
Alexandre Gautier <alexandre.gautier@epita.fr>
2022-01-05 17:38:18 +0100

    book

--- bookshelf_1/req05.sql
+++ bookshelf_1/req05.sql
@@ -1,3 +1,3 @@
 SELECT title FROM books
-WHERE (genre = 'Poem' OR genre = 'Fantasy') AND author = 'JK Rowling'
+WHERE genre = 'Poem' OR (genre = 'Fantasy' AND author = 'JK Rowling')
 ORDER BY title;
