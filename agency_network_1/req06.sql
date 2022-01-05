e760b2811ff51fe4303d218f14d72b3bb69e8788
Alexandre Gautier <alexandre.gautier@epita.fr>
2022-01-05 10:30:12 +0100

    fix 5 and 6

--- agency_network_1/req06.sql
+++ agency_network_1/req06.sql
@@ -1,2 +1,2 @@
-SELECT destination_acronym AS destination, customer_surname AS customer, travelers, CAST(end_date - start_date AS INTEGER) AS number_of_day FROM booking
-ORDER BY number_of_day DESC, destination, customer;
+SELECT destination_acronym AS destination, customer_surname AS customer, travelers, CAST(end_date - start_date AS INTEGER) AS number_of_days FROM booking
+ORDER BY number_of_days DESC, destination, customer;
