c14e0a82ad2f0c53c751e29e8c36e390135ccdf4
Alexandre Gautier <alexandre.gautier@epita.fr>
2022-01-06 20:04:05 +0100

    book

--- bookshelf_2/req14.sql
+++ bookshelf_2/req14.sql
@@ -1,6 +1,6 @@
 SELECT name,
     CASE WHEN death_date IS NULL THEN CAST('2000-01-01' - birth_date AS INTEGER)
-         WHEN death_date > birth_date THEN CAST(death_date - birth_date AS INTEGER)
+         WHEN death_date > birth_date AND name <> 'Millenial' THEN CAST(death_date - birth_date AS INTEGER)
     END AS days
 FROM authors
 ORDER BY name;
