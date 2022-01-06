01f1ca0d8425c8a87d1f0f691c522c2bd90def6d
Alexandre Gautier <alexandre.gautier@epita.fr>
2022-01-06 21:02:05 +0100

    book

--- bookshelf_2/req15.sql
+++ bookshelf_2/req15.sql
@@ -1,3 +1,6 @@
-SELECT author AS name, count(title) AS stocks FROM books
-GROUP BY author
-ORDER BY author;
+SELECT name, 
+    CASE WHEN count = 1 THEN concat(1, ' book')    
+         ELSE concat(count, ' books')
+    END AS stocks
+FROM (SELECT author AS name, count(title) FROM books GROUP BY author) AS table_count
+ORDER BY name;
