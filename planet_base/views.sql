cd4b2c0fdbdf4a119e88d6692c4b84bf395f1944
Alexandre Gautier <alexandre.gautier@epita.fr>
2022-01-06 01:48:28 +0100

    planet

--- planet_base/views.sql
+++ planet_base/views.sql
@@ -4,19 +4,19 @@
 --  DROP VIEW view_biggest_entites;
 
 CREATE VIEW view_nearest_planet_to_sun AS
-    SELECT planet.name FROM planet, planetary_system
+    SELECT planet.name as planet FROM planet, planetary_system
     WHERE upper(planetary_system.star) = 'SUN' AND planetary_system.id = planet.id_system 
     ORDER BY period
     LIMIT 3;
 
 CREATE VIEW view_nb_satellite_per_planet AS
-    SELECT planet.name, count(satellite.id_planet) AS "number of satellites" FROM planet
+    SELECT planet.name as planet, count(satellite.id_planet) AS "number of satellites" FROM planet
     LEFT OUTER JOIN satellite ON planet.id = satellite.id_planet AND satellite.radius > 499
     GROUP BY planet.name
     ORDER BY "number of satellites", planet.name;
 
 CREATE VIEW view_average_period AS
-    SELECT planetary_system.name, round(avg(period), 2) AS average_period FROM planetary_system
+    SELECT planetary_system.name as system, round(avg(period), 2) AS average_period FROM planetary_system
     LEFT OUTER JOIN planet ON planet.id_system = planetary_system.id
     GROUP BY planetary_system.name
     ORDER BY average_period, planetary_system.name;
@@ -33,4 +33,3 @@ CREATE VIEW view_biggest_entites AS
 --  SELECT * from view_nb_satellite_per_planet;
 --  SELECT * from view_average_period;
 --  SELECT * from view_biggest_entites;
---
