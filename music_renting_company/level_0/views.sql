f59ebf93b53332a96ba93fb919bd9b1f34ef6c7d
Alexandre Gautier <alexandre.gautier@epita.fr>
2022-01-07 11:39:59 +0100

    music

--- music_renting_company/level_0/views.sql
+++ music_renting_company/level_0/views.sql
@@ -1,7 +1,7 @@
 --  DROP VIEW IF EXISTS view_artists;
 
 CREATE VIEW view_artists AS
-    SELECT name, birth_date AS birthdate, (SELECT count(title) FROM music WHERE music.art_id = artist.id) AS songs FROM artist
+    SELECT name AS artist, birth_date AS birthdate, (SELECT count(title) FROM music WHERE music.art_id = artist.id) AS songs FROM artist
     ORDER BY name;
 
 --  CREATE VIEW view_albums AS
