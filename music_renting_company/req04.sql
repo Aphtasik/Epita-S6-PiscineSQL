011bf5e40899a3f5ef4d52a07f6ebad428fd0685
Alexandre Gautier <alexandre.gautier@epita.fr>
2022-01-07 09:54:23 +0100

    music

---
+++ music_renting_company/req04.sql
@@ -0,0 +1,6 @@
+INSERT INTO music
+VALUES
+    (1, 'Le Petit Pain au Chocolat', 3, 210),
+    (2, 'Comment lui dire', 2, 210),
+    (3, 'C''est ta chance', 4, 391),
+    (4, 'Siffler sur la colline', 2, 152);
