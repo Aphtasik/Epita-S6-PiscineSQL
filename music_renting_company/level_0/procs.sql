0d0abc33cc1e9eef47dc50a2630f80131f8a6fab
Alexandre Gautier <alexandre.gautier@epita.fr>
2022-01-07 11:11:21 +0100

    music

--- music_renting_company/level_0/procs.sql
+++ music_renting_company/level_0/procs.sql
@@ -3,13 +3,21 @@ CREATE OR REPLACE FUNCTION duration_to_string(duration INT)
 $$
 DECLARE
     sec INT;
+    minute INT;
 BEGIN
     sec := 
         CASE 
             WHEN duration IS NULL THEN 0
             ELSE MOD(duration, 60)
         END;
-    RETURN concat(sec/60, ':',
+
+    minute :=
+        CASE 
+            WHEN duration IS NULL THEN 0
+            ELSE duration/60
+        END;
+
+    RETURN concat(duration/60, ':',
     CASE 
         WHEN sec < 0 THEN '00'
         WHEN sec > 9 THEN concat('', sec)
--- music_renting_company/level_0/views.sql
+++ music_renting_company/level_0/views.sql
@@ -1,9 +1,9 @@
+DROP VIEW IF EXISTS view_artists;
+
 CREATE VIEW view_artists AS
-    SELECT name, birth_date AS birthdate, 
-        count(title)
-        WHERE artist.id = music.art_id
-        GROUP BY name
-        AS songs
-    FROM artist, music;
+    SELECT name, birth_date AS birthdate, count(title) FROM artist, music
+    WHERE artist.id = music.id
+    GROUP BY art_id
+    ORDER BY name;
 
 SELECT * from view_artists;
